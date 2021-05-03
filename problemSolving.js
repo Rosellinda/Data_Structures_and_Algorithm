2eb004649478ce7e99a2e4d4d0fba8b87570b06c
Rosellinda <64804606+Rosellinda@users.noreply.github.com>
2021-05-03 23:56:21 +0800

    Frequency Counters Pattern

--- problemSolving.js
+++ problemSolving.js
@@ -51,24 +51,63 @@
 // }
 
 
-function charCount(str){
-    //make object to return at end
-    let result = {};
-    //loop over string, for each character...
+// function charCount(str){
+//     //make object to return at end
+//     let result = {};
+//     //loop over string, for each character...
+//     for(let i = 0; i < str.length; i++){
+//         let char = str[i].toLowerCase();
+//         //if the char is a number/letter AND is a key in object, add one to count
+//         if(result[char] > 0){
+//             result[char]++;
+//         }
+//         //If the char is a number/letter AND not in object, add it to object and set value to 1
+//         else {
+//             result[char] = 1
+//         };
+//     }
+//         //if character is something else (space, period, etc.) don't do anything
+//     //return object at end
+//     console.log(result);
+// }
+// countString("Hello");
+
+// function charCount(str) {
+//     let obj = {};
+//     for(let i = 0; i < str.length; i++){
+//         let char = str[i].toLowerCase();
+//         if(/[a-z0-9]/.test(char)){
+//             if(obj[char] > 0){
+//                 obj[char]++;
+//             } else {
+//                 obj[char] = 1; 
+//             };
+//         }
+//     }
+//     console.log(obj);
+// }
+
+function charCount(str) {
+    let obj = {};
     for(let i = 0; i < str.length; i++){
         let char = str[i].toLowerCase();
-        //if the char is a number/letter AND is a key in object, add one to count
-        if(result[char] > 0){
-            result[char]++;
+        if(isAlphaNumeric(char)){
+            obj[char] = ++obj[char] || 1;
         }
-        //If the char is a number/letter AND not in object, add it to object and set value to 1
-        else {
-            result[char] = 1
-        };
     }
-        //if character is something else (space, period, etc.) don't do anything
-    //return object at end
-    console.log(result);
+    console.log(obj);
 }
-// countString("Hello");
+
+
+function isAlphaNumeric(char) {
+    let code = char.charCodeAt(0);
+    if(!(code > 47 && code < 58) && //numeric(0-9)
+        !(code > 64 && code < 91) && //upper alpha (A-Z)
+        !(code > 96 && code < 123)){ //lower alpha (a-z)
+        return false;
+    }
+    return true;
+}
+
 charCount("Hello");
+
